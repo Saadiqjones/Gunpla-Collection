012313c2af69d982c0f726c0f49cdc39504222b2
sjones <jones.saadiq@gmail.com>
2025-03-04 18:56:46 -0500

    Gunpla-001 | setup

---
+++ src/main/java/com/saj/mcip/gunplacollection/GunplaCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.saj.mcip.gunplacollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunplaCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GunplaCollectionApplication.class, args);
+    }
+
+}
